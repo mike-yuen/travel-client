35f2a66e5d151427e3c0351132a518da240c4ae0
letranhuudanh0030 <letranhuudanh0030@gmail.com>
2020-02-21 19:26:42 +0700

    update HotelCard v2

--- src/views/Hotels.vue
+++ src/views/Hotels.vue
@@ -17,22 +17,43 @@
           <div v-else>
             <div v-for="(hotel, index) in hotels" :key="index">
               <HotelCard>
-                <img slot="figure" :src="hotel.hotelImage" alt="" class="w-full" >
-                <h3 slot="title" class="uppercase font-bold text-2xl mb-2">{{ hotel.title }}</h3>
-                <p slot="country" class="text-gray-500 capitalize mb-3">{{ hotel.country }}</p>
-                <div slot="numberOfRank" v-for="index in hotel.numberOfRank" :key="index">
+                <img slot="figure" :src="hotel.hotelImage" alt class="w-full" />
+                <h3 slot="title" class="uppercase font-bold text-2xl mb-2">
+                  {{ hotel.title }}
+                </h3>
+                <p slot="country" class="text-gray-500 capitalize mb-3">
+                  {{ hotel.country }}
+                </p>
+                <div
+                  slot="numberOfRank"
+                  v-for="index in hotel.numberOfRank"
+                  :key="index"
+                >
                   <div class="rounded-full bg-yellow-500 h-4 w-4 mr-1"></div>
                 </div>
-                <img slot="tripadvisorImage" :src="hotel.tripadvisorImage" alt="" class="w-100 mr-1">
-                <span slot="numberOfReviews" class="text-gray-500 self-center">{{ hotel.numberOfReviews }} reviews</span>
-                <div slot="icon" v-for="(icon, index) in hotel.icons" :key="index">
-                  <div class="rounded-full h-10 w-10 flex items-center justify-center bg-gray-300 mr-2 mt-10">
+                <img
+                  slot="tripadvisorImage"
+                  :src="hotel.tripadvisorImage"
+                  alt
+                  class="w-100 mr-1"
+                />
+                <span slot="numberOfReviews" class="text-gray-500 self-center"
+                  >{{ hotel.numberOfReviews }} reviews</span
+                >
+                <div
+                  slot="icon"
+                  v-for="(icon, index) in hotel.icons"
+                  :key="index"
+                >
+                  <div
+                    class="rounded-full h-10 w-10 flex items-center justify-center bg-gray-300 mr-2 mt-10"
+                  >
                     <i :class="'fa fa-' + icon"></i>
                   </div>
                 </div>
                 <p slot="numberOfNights">
                   <span class>{{ hotel.numberOfNights }} night</span>
-                    from (AUD)
+                  from (AUD)
                 </p>
                 <p slot="discount" class="line-through text-gray-500">
                   ${{ hotel.discount }}
