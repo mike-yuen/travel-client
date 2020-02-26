1f41ab1c80b0d498390300030dde4143454a8507
minhnguyen1505 <nhatminh.150596@gmail.com>
2020-02-26 21:25:16 +0700

    ssr testing

--- src/views/HotelProperties.vue
+++ src/views/HotelProperties.vue
@@ -1,20 +1,67 @@
 <template>
   <div :id="'hotel-properties' + hotelId">
     {{ hotelId }}
+    <div class="relative w-full max-w-5lg mx-auto">
+      <RoomCardGeneral :data="dataRoom" />
+    </div>
   </div>
 </template>
 
 <script>
+import RoomCardGeneral from "@/components/room-card/RoomCardGeneral";
 export default {
   name: "HotelProperties",
+  components: {
+    RoomCardGeneral
+  },
   data() {
     return {
-      hotelId: ""
+      hotelId: "",
+      dataRoom: {
+        name: "No More F*cking Mock, 1 King Bed",
+        image_url:
+          "https://i.travelapi.com/hotels/20000000/19480000/19473700/19473638/a07ad6ed_z.jpg",
+        adults: 2,
+        description: {
+          "0": "1 Queen Bed",
+          "1": "269-sq-foot (25-sq-meter) room with courtyard views",
+          "2": "",
+          "3": "Internet - Free WiFi ",
+          "4": "Entertainment - Flat-screen TV with cable channels",
+          "5":
+            "Food & Drink - Kitchenette with refrigerator, stovetop, and cookware/dishware",
+          "6":
+            "Bathroom - Shower, free toiletries, and a hair dryer (on request)",
+          "7": "Practical - Desk",
+          "8": "Comfort - Air conditioning",
+          "9": "Need to Know - Limited housekeeping",
+          "10": "Non-Smoking"
+        },
+        features: {
+          "0": "Non-Smoking",
+          "1": "Free toiletries",
+          "2": "Free WiFi",
+          "3": "Shower only",
+          "4": "Hair dryer (on request)",
+          "5": "Stovetop",
+          "6": "Cookware, dishware, and utensils",
+          "7": "Room service",
+          "8": "Limited housekeeping",
+          "9": "Flat-panel TV",
+          "10": "Cable TV service",
+          "11": "Air conditioning",
+          "12": "Desk",
+          "13": "Kitchenette",
+          "14": "Refrigerator"
+        },
+        prices: 234
+      }
     };
   },
   created() {
     const params = this.$route.params;
     this.hotelId = params.hotelId;
+    // then fetch api...
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
   runtimeCompiler: true,
-  transpileDependencies: ["vue-agile", "vuex-persist"]
+  transpileDependencies: ["vue-agile", "vuex-persist"],
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, "dist"),
+        // Required - Routes to render.
+        routes: ["/", "/hotels"]
+      })
+    ]
+  }
 };
