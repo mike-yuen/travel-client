40f025b0fdd2a6175c36add82ca36ff908e8a9c2
minhnguyen1505 <nhatminh.150596@gmail.com>
2020-02-28 09:28:26 +0700

    rm ssr

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,11 @@
-const path = require("path");
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-
 module.exports = {
   runtimeCompiler: true,
   transpileDependencies: ["vue-agile", "vuex-persist"],
   configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, "dist"),
-        routes: ["/"],
-        postProcess(renderedRoute) {
-          // Ignore any redirects.
-          renderedRoute.route = renderedRoute.originalRoute;
-          renderedRoute.html = renderedRoute.html
-            .replace(/<script (.*?)>/g, "<script $1 defer>")
-            .replace('id="app"', 'id="app" data-server-rendered="true"');
-          return renderedRoute;
-        },
-        minify: {
-          collapseBooleanAttributes: true,
-          collapseWhitespace: true,
-          decodeEntities: true,
-          keepClosingSlash: true,
-          minifyCSS: true,
-          minifyJS: true,
-          sortAttributes: true
-        }
-      })
-    ]
+    optimization: {
+      splitChunks: {
+        chunks: "all"
+      }
+    }
   }
 };
