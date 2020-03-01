5d75fb39d3255bf009f1e9325b6b2cc5f847b004
letranhuudanh0030 <letranhuudanh0030@gmail.com>
2020-03-01 09:41:59 +0700

    create core UI

--- src/views/Hotels.vue
+++ src/views/Hotels.vue
@@ -1,20 +1,36 @@
 <template>
-  <div id="hotels">
-    <HotelCard
-      v-for="(hotel, index) in hotels"
-      :key="index"
-      :hotel="hotel"
-      :loading="loading"
-    />
+  <div>
+    <!-- <NavigatorTop /> -->
+    <!-- <MenuDesktop /> -->
+    <div class="lg:flex container mx-auto">
+      <div class="w-3/12">
+        <HotelRatingFilter />
+      </div>
+      <div class="w-12/12 lg:w-9/12">
+        <HotelCard
+          v-for="(hotel, index) in hotels"
+          :key="index"
+          :hotel="hotel"
+          :loading="loading"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// import NavigatorTop from "@/components/navigator/NavigatorTop";
+// import MenuDesktop from "@/components/menu/MenuDesktop";
 import HotelCard from "@/components/hotel-card/HotelCard";
+import HotelRatingFilter from "@/components/advanced-filter/HotelRating";
+
 export default {
   name: "Hotels",
   components: {
-    HotelCard
+    // NavigatorTop,
+    // MenuDesktop,
+    HotelCard,
+    HotelRatingFilter
   },
   data() {
     return {
@@ -25,8 +41,7 @@ export default {
           country: "VietNam",
           tripadvisorImage:
             "https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.5-15969-4.svg",
-          hotelImage:
-            "https://i.travelapi.com/hotels/3000000/2060000/2057700/2057692/d1374aea_b.jpg",
+          hotelImage: require("@/assets/images/room-test.jpg"),
           url: "void:javascript(0)",
           discount: 300,
           price: 200,
@@ -42,8 +57,7 @@ export default {
           country: "US",
           tripadvisorImage:
             "https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.5-15969-4.svg",
-          hotelImage:
-            "https://i.travelapi.com/hotels/3000000/2060000/2057700/2057692/d1374aea_b.jpg",
+          hotelImage: require("@/assets/images/room-test.jpg"),
           url: "void:javascript(0)",
           discount: 300,
           price: 200,
@@ -55,12 +69,23 @@ export default {
           icons: ["book", "car"]
         }
       ]
+      // rates: [
+      //   {
+      //       id: '1',
+      //       lable: 'checkbox1'
+      //   },
+      //   {
+      //       id: '2',
+      //       lable: 'checkbox2'
+      //   }
+      // ]
     };
   },
   methods: {
     skeletonLoading() {
       setTimeout(() => {
         this.loading = !this.loading;
+        // this.loading = true;
       }, 2000);
     }
   },
